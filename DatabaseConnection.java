b7fcf2c894511ae11f3e025afbebb8b9c1e73ebb
Chaitanya Malik <chaitanyamalik936@gmail.com>
2026-06-19 20:18:39 +0530

    Remove database password from source code

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/lib";
     private static final String USER = "root";
-    private static final String PASSWORD = "mysql123";
+    private static final String PASSWORD = "your_password";
 
     public static Connection getConnection() {
         try {
